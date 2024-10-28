5e37a7a7934103a1d6cedb5cdedd3e084328598b
Jun Rhy Crodua <jrcrodua@gmail.com>
2024-10-28 15:35:07 +0800

    transportation enhancement

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -71,24 +71,47 @@ export default function Authenticated({
                                         </Dropdown.Content>
                                     </Dropdown>
                                 </div>
-                                <NavLink
+                                {/* <NavLink
                                     href={route('inventory')}
                                     active={route().current('inventory')}
                                 >
                                     Inventory
-                                </NavLink>
-                                <NavLink
-                                    href={route('warehousing')}
-                                    active={route().current('warehousing')}
-                                >
-                                    Warehousing
-                                </NavLink>
-                                <NavLink
-                                    href={route('transportation')}
-                                    active={route().current('transportation')}
-                                >
-                                    Transportation
-                                </NavLink>
+                                </NavLink> */}
+                                <div className="inline-flex items-center">
+                                    <Dropdown>
+                                        <Dropdown.Trigger>
+                                            <span className="inline-flex rounded-md">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                                >
+                                                    <span className="mt-[1px]">Distribution</span>
+                                                    <svg
+                                                        className="ml-2 -mr-0.5 h-4 w-4"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20"
+                                                        fill="currentColor"
+                                                    >
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clipRule="evenodd"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </Dropdown.Trigger>
+
+                                        <Dropdown.Content>
+                                            <Dropdown.Link href={route('warehousing')}>
+                                                Warehousing
+                                            </Dropdown.Link>
+                                            <Dropdown.Link href={route('transportation')}>
+                                                Distribution
+                                            </Dropdown.Link>
+                                        </Dropdown.Content>
+                                    </Dropdown>
+                                </div>
                                 <div className="inline-flex items-center">
                                     <Dropdown>
                                         <Dropdown.Trigger>
