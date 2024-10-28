334e29ce8f4f9134572621f2138a813400bf1e3c
Jun Rhy Crodua <jrcrodua@gmail.com>
2024-10-28 19:46:04 +0800

    Pos retails products from backend

--- app/Http/Controllers/PosRetailController.php
+++ app/Http/Controllers/PosRetailController.php
@@ -12,7 +12,17 @@ class PosRetailController extends Controller
      */
     public function index()
     {
-        return Inertia::render('PosRetail');
+        $products = [
+            ['id' => 1, 'name' => "Product A", 'price' => 10.0, 'images' => ["/images/product-a.jpg"], 'quantity' => 50],
+            ['id' => 2, 'name' => "Product B", 'price' => 15.0, 'images' => ["/images/product-b.jpg"], 'quantity' => 30],
+            ['id' => 3, 'name' => "Product C", 'price' => 20.0, 'images' => ["/images/product-c.jpg"], 'quantity' => 20],
+            ['id' => 4, 'name' => "Product D", 'price' => 25.0, 'images' => ["/images/product-d.jpg"], 'quantity' => 15],
+            ['id' => 5, 'name' => "Product E", 'price' => 30.0, 'images' => ["/images/product-e.jpg"], 'quantity' => 25],
+            ['id' => 6, 'name' => "Product F", 'price' => 35.0, 'images' => ["/images/product-f.jpg"], 'quantity' => 10],
+            ['id' => 7, 'name' => "Product G", 'price' => 40.0, 'images' => ["/images/product-g.jpg"], 'quantity' => 5],
+        ];
+    
+        return Inertia::render('PosRetail', ['products' => $products]);
     }
 
     /**
