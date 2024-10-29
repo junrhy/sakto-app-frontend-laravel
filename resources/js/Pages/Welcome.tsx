ad20a3c9a21beeb681d5603bfe16ea1350968eb3
Jun Rhy Crodua <jrcrodua@gmail.com>
2024-10-29 11:31:58 +0800

    Login page ui enhancement

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -51,9 +51,8 @@ export default function Welcome({
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <div className="text-center">
                             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
-                                <span className="block">Transform Your Work with</span>
-                                <span className="block text-indigo-600">Sakto</span>
-                                business apps.
+                                <span className="block">Innovate Your Business with</span>
+                                <span className="text-indigo-600">Sakto</span> management apps.
                             </h1>
                             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 dark:text-gray-400 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                                 Streamline your workflow, boost productivity, and achieve better results with our powerful platform.
