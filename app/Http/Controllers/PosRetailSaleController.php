0cd9f8fb61a2d713d17273e751a68093ed6a20fc
Jun Rhy Crodua <jrcrodua@gmail.com>
2024-11-06 00:05:05 +0800

    load backend sales data

--- app/Http/Controllers/PosRetailSaleController.php
+++ app/Http/Controllers/PosRetailSaleController.php
@@ -9,9 +9,31 @@ use Illuminate\Support\Facades\Log;
 
 class PosRetailSaleController extends Controller
 {
+    protected $apiUrl, $apiToken;
+
+    public function __construct()
+    {
+        $this->apiUrl = env('API_URL');
+        $this->apiToken = env('API_TOKEN');
+    }
+
     public function index()
     {
-        return Inertia::render('PosRetailSale');
+        // Fetch data from API
+        $response = Http::withToken($this->apiToken)
+            ->get("{$this->apiUrl}/pos-retail/sales");
+
+        if(!$response->json()) {
+            return response()->json(['error' => 'Failed to connect to Retail POS Sales API.'], 500);
+        }
+
+        if (!$response->successful()) {
+            throw new \Exception('API request failed: ' . $response->body());
+        }
+
+        return Inertia::render('PosRetailSale', [
+            'sales' => $response->json()['data']
+        ]);
     }
 
     public function showSalesReport() {
