8383a70bb6cd4ece8f7accc41c81592a91e7e618
Jun Rhy Crodua <jrcrodua@gmail.com>
2024-12-03 21:20:16 +0800

    rental items

--- app/Http/Controllers/RentalPropertyController.php
+++ app/Http/Controllers/RentalPropertyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+
 class RentalPropertyController extends Controller
 {
     protected $apiUrl, $apiToken;
