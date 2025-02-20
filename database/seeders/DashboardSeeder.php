9664e981b908009a064d168afe4fcdb0f1664df5
Jun Rhy Crodua <jrcrodua@gmail.com>
2025-02-20 12:11:39 +0800

    add User model

--- database/seeders/DashboardSeeder.php
+++ database/seeders/DashboardSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Dashboard;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class DashboardSeeder extends Seeder
